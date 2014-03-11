236ec54825e1cd9aa264dfa2d87b2ac9c5b897fa
Stephan McLean <Stephan@Stephans-MacBook-Air.local>
2014-03-11 14:02:18 +0000

    Basic event handling implemented

--- btv/event/torrent/TorrentEvent.java
+++ btv/event/torrent/TorrentEvent.java
@@ -1,3 +1,11 @@
+/*
+	This class will be used to relay torrent information to
+	a client which is using TorrentListeners.
+
+	Author: Stephan McLean
+	Date: 11th March 2014
+*/
+
 package btv.event.torrent;
 
 import java.util.EventObject;
--- btv/event/torrent/TorrentListener.java
+++ btv/event/torrent/TorrentListener.java
@@ -1,3 +1,12 @@
+/*
+	Classes that realize this interface can add a listener to a Torrent
+	to receive information about the Torrent periodically.
+
+	Author: Stephan McLean
+	Date: 11th March 2014.
+
+*/
+
 package btv.event.torrent;
 
 import java.util.EventListener;
