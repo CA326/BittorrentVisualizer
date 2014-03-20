858965f578872c722e637a37dfb699c0578fd805
Stephan McLean <Stephan@Stephans-MacBook-Air.local>
2014-03-20 14:26:38 +0000

    Bencoding tests complete.

--- btv/tests/bencoding/BDecoderTestCase.java
+++ btv/tests/bencoding/BDecoderTestCase.java
@@ -4,39 +4,79 @@ import btv.bencoding.BDecoder;
 import btv.bencoding.BDecodingException;
 
 import org.junit.Test;
+import org.junit.Rule;
 import static org.junit.Assert.assertEquals;
+import org.junit.rules.ExpectedException;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+/*	
+	Test case for class BDecoder.
+
+	Author: Stephan McLean
+
+*/
 public class BDecoderTestCase {
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 
 	public BDecoderTestCase() {}
 
 	@Test
-	public void testDecodeString() {
-
+	public void testDecodeString() throws BDecodingException {
+		String toDecode = "4:spam";
+		String expected = "spam";
+		
+		String result = (String) BDecoder.decode(toDecode);
+		assertEquals(result, expected);
 	}
 
 	@Test
-	public void testDecodeInt() {
-
+	public void testDecodeInt() throws BDecodingException {
+		String toDecode = "i42e";
+		int expected = 42;
+		
+		int	result = (int) BDecoder.decode(toDecode);
+		assertEquals(result, expected);
 	}
 
 	@Test
-	public void testDecodeList() {
-
+	public void testDecodeList() throws BDecodingException {
+		String toDecode = "l4:spam4:eggse";
+		ArrayList expected = new ArrayList();
+		expected.add("spam"); expected.add("eggs");
+		
+		ArrayList result = (ArrayList) BDecoder.decode(toDecode);
+		assertEquals(result, expected);
 	}
 
 	@Test
-	public void testDecodeMap() {
-
+	public void testDecodeMap() throws BDecodingException {
+		String toDecode = "d3:cow3:moo4:spam4:eggse";
+		Map expected = new LinkedHashMap(); // new Map() ?
+		expected.put("cow", "moo");
+		expected.put("spam", "eggs");
+		
+		Map	result = (Map) BDecoder.decode(toDecode);
+		assertEquals(result, expected);
 	}
 
 	@Test
-	public void testUnexpectedEOF() {
-
+	public void testUnexpectedEOF() throws BDecodingException {
+		thrown.expect(BDecodingException.class);
+		thrown.expectMessage("Unexpected end of file.");
+		String toDecode = "4:sp";
+		String result = (String) BDecoder.decode(toDecode);
 	}
 
 	@Test
-	public void testUnexpectedChar() {
-
+	public void testUnexpectedChar() throws BDecodingException {
+		String toDecode = "b1234e";
+		thrown.expect(BDecodingException.class);
+		thrown.expectMessage("Unable to process item: b");
+		BDecoder.decode(toDecode);
 	}
 }
--- btv/tests/bencoding/BEncoderTestCase.java
+++ btv/tests/bencoding/BEncoderTestCase.java
@@ -5,32 +5,57 @@ import btv.bencoding.BEncoder;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+/*
+	Test case for class BEncoder.
+
+	Author: Stephan McLean
+
+*/
 public class BEncoderTestCase {
 
 	public BEncoderTestCase() {}
 
 	@Test
 	public void testEncodeString() {
+		String toEncode = "spam";
+		String expected = "4:spam";
 
+		String result = BEncoder.bencode(toEncode);
+		assertEquals(result, expected);
 	}
 
 	@Test
 	public void testEncodeInt() {
+		int toEncode = 42;
+		String expected = "i42e";
 
+		String result = BEncoder.bencode(toEncode);
+		assertEquals(result, expected);
 	}
 
 	@Test
 	public void testEncodeList() {
+		ArrayList toEncode = new ArrayList();
+		toEncode.add("spam");
+		toEncode.add("eggs");
+		String expected = "l4:spam4:eggse";
 
+		String result = BEncoder.bencode(toEncode);
+		assertEquals(result, expected);
 	}
 
 	@Test
 	public void testEncodeMap() {
+		Map toEncode = new LinkedHashMap();
+		toEncode.put("cow", "moo");
+		toEncode.put("spam", "eggs");
+		String expected = "d3:cow3:moo4:spam4:eggse";
 
-	}
-
-	@Test
-	public void testEncodeNotSupported() {
-
+		String result = BEncoder.bencode(toEncode);
+		assertEquals(result, expected);
 	}
 }
